9a47721d8e176fbf25640681937f27e5f66a39d9
Nikos Cocci <coccinikos@gmail.com>
2020-11-21 19:50:00 +0100

    made landing page responsive, removed white background from images

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -11,7 +11,7 @@ function Landing () {
 
 
   return (
-    <div data-testid="landing">
+    <div data-testid="landing" className="landing">
       <div className="landing-cover">
 
         <div className="header">
@@ -137,12 +137,12 @@ function Landing () {
 
       <div className="footer">
         <div className="footer-content">
-          <div className="credits">
-            <div>ABOUT THIS MAP APPLICATION</div>
+          <div className="footer-div">
+            <div  className="footer-title">ABOUT THIS MAP APPLICATION</div>
             <div>This is a project for Catcalls of Amsterdam by Lisanne Kraal. Inspired by the young activists, she contacted the organization to help them mapping their enormous database in an interactive way. This is a first version of a project under construction. Any feedback or want to contribute? Contact us.</div>
           </div>
-          <div className="contact">
-            <div>CONTACT</div>
+          <div className="footer-div">
+            <div className="footer-title">CONTACT</div>
             <div>Catcalls of Amsterdam</div>
 
             <div>
