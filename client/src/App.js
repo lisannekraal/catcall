04ecf6714fab432a0830b76d8adbeae47c29f3d4
serendatapy <arvaldivia@gmail.com>
2020-11-23 09:34:13 +0100

    feat(added responsive nav bar replacing non responsive one): the new navbar implements a hamburger

--- client/src/App.js
+++ client/src/App.js
@@ -9,9 +9,13 @@ import Landing from './components/Landing';
 import ReportForm from './components/ReportForm';
 import LoginModal from './components/Login-Modal';
 import Dashboard from './components/Dashboard';
-import NotFound from './components/Not-found'
+import NotFound from './components/Not-found';
 
-// import Navbar from './components/Navbar';
+import Header from './components/Header';
+
+
+import Navbar from './components/Navbar';
+import SideDrawer from './components/SideDrawer';
 
 const client = new ApolloClient({
   uri: process.env.REACT_APP_APOLLO_SERVER,
@@ -27,7 +31,7 @@ function App() {
   return (
     <ApolloProvider client={client}>
       <Router>
-      <div className="navbar" data-testid="navbar" >
+      {/* <div className="navbar" data-testid="navbar" >
         <div className="navbar-content">
           <Link style={{ textDecoration: 'none', color: 'white' }} to="/">
             <div className="navbar-brand">
@@ -49,8 +53,6 @@ function App() {
             </div>
             <div className="navbar-login">
               <LoginModal />
-              {/* <i className="fas fa-user-cog"></i> MODERATORS */}
-              {/* <Link style={{ textDecoration: 'none', color: 'white' }} to="/"><i className="fas fa-user-cog"></i> MODERATORS</Link> */}
             </div>
             <div className="navbar-report">
               <Link to="/catcalls/new"><button><p>Report a new datcall</p></button></Link>
@@ -58,8 +60,9 @@ function App() {
 
           </div>
         </div>
-      </div>
-      {/* <Navbar/> */}
+      </div> */}
+      <Header/>
+
 
         <Switch>
           <Route exact path="/">
---
+++ client/src/components/SideDrawer.js
@@ -0,0 +1,80 @@
+import React, { useState } from 'react'
+import { IconButton, List, ListItem, ListItemText, Drawer, ListItemIcon,Typography } from "@material-ui/core"
+import { Menu } from "@material-ui/icons"
+
+import { useHistory } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+import logo from '../assets/logowhite.png'
+import Button from '@material-ui/core/Button';
+
+
+const useStyles = makeStyles({
+  list: {
+    width: 250,
+  },
+  linkText: {
+    textDecoration: `none`,
+    textTransform: `uppercase`,
+    color: `black`,
+  },
+})
+
+function SideDrawer({ navLinks }) {
+
+  let history = useHistory();
+  const classes = useStyles();
+
+  const [drawerState, setDrawerState] = useState({ right: false })
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return
+    }
+    setDrawerState({ [anchor]: open })
+  }
+
+  const sideDrawerList = anchor => (
+    <div
+      className={classes.list}
+      role="presentation"
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
+      <List component="nav">
+      <ListItem button onClick={() => history.push("/catcalls/new")} style={{background: 'rgb(245, 37, 89)'}}>
+      <ListItemIcon className="fas fa-cat"/>
+      <ListItemText  primary={<Typography variant="button">Report a new CatCall</Typography>}/>
+      </ListItem>
+        {navLinks.map(({ title, path, classN }) => (
+          <ListItem button onClick={path}>
+            <ListItemIcon className={classN}/>
+            <ListItemText primary={<Typography variant="button">{title}</Typography>}/>
+          </ListItem>
+        ))}
+      </List>
+    </div>
+  );
+
+
+  return (
+    <React.Fragment>
+
+      <IconButton edge="start" aria-label="menu" onClick={toggleDrawer("right", true)} >
+        <Menu fontSize="large" style={{ color: `white` }}/>
+      </IconButton>
+      <Drawer
+        anchor="right"
+        open={drawerState.right}
+        onOpen={toggleDrawer("right", true)}
+        onClose={toggleDrawer("right", false)}
+      >
+        {sideDrawerList("right")}
+      </Drawer>
+
+    </React.Fragment>
+  )
+}
+
+export default SideDrawer
