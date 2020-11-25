626f23c771d4d98b0c249f09f3c136e4459369d3
serendatapy <arvaldivia@gmail.com>
2020-11-25 09:46:44 +0100

    docs(learning to use table): doing tests

--- client/src/components/AdminTable.js
+++ client/src/components/AdminTable.js
@@ -23,7 +23,11 @@ const useRowStyles = makeStyles({
   },
 });
 
-function createData(name, calories, fat, carbs, protein, price) {
+/**
+ * Function labels data in the table
+ */
+function createData(name, calories, fat, carbs, protein, price, history) {
+  console.log(arguments)
   return {
     name,
     calories,
@@ -31,12 +35,45 @@ function createData(name, calories, fat, carbs, protein, price) {
     carbs,
     protein,
     price,
-    history: [
-      { date: '2020-01-05', customerId: '11091700', amount: 3 },
-      { date: '2020-01-02', customerId: 'Anonymous', amount: 1 },
-    ],
+    history: [...history],
   };
 }
+
+const rows = [
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0, 3.99,[{ date: '2020-01-05', customerId: 'Markus', amount: 3 },{ date: '2020-01-02', customerId: 'Mark', amount: 1 }]),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3, 4.99,[{ date: '2020-01-05', customerId: 'Jarkus', amount: 3 },{ date: '2020-01-02', customerId: 'Jark', amount: 1 }]),
+  createData('Eclair', 262, 16.0, 24, 6.0, 3.79,[{ date: '2020-01-05', customerId: 'Larkus', amount: 3 },{ date: '2020-01-02', customerId: 'Lark', amount: 1 }]),
+  createData('Cupcake', 305, 3.7, 67, 4.3, 2.5,[{ date: '2020-01-05', customerId: 'Markus', amount: 3 },{ date: '2020-01-02', customerId: 'Mark', amount: 1 }]),
+  createData('Gingerbread', 356, 16.0, 49, 3.9, 1.5,[{ date: '2020-01-05', customerId: 'Markus', amount: 3 },{ date: '2020-01-02', customerId: 'Mark', amount: 1 }]),
+];
+
+export default function CollapsibleTable() {
+  return (
+    <TableContainer component={Paper}>
+      <Table aria-label="collapsible table">
+        <TableHead>
+          <TableRow>
+            <TableCell />
+            <TableCell>Dessert (100g serving)</TableCell>
+            <TableCell align="right">Calories</TableCell>
+            <TableCell align="right">Fat&nbsp;(g)</TableCell>
+            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <Row key={row.name} row={row} />
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
+
+/**
+ * Each Row component takes a row of data
+ */
 
 function Row(props) {
   const { row } = props;
@@ -97,53 +134,23 @@ function Row(props) {
     </React.Fragment>
   );
 }
-
-Row.propTypes = {
-  row: PropTypes.shape({
-    calories: PropTypes.number.isRequired,
-    carbs: PropTypes.number.isRequired,
-    fat: PropTypes.number.isRequired,
-    history: PropTypes.arrayOf(
-      PropTypes.shape({
-        amount: PropTypes.number.isRequired,
-        customerId: PropTypes.string.isRequired,
-        date: PropTypes.string.isRequired,
-      }),
-    ).isRequired,
-    name: PropTypes.string.isRequired,
-    price: PropTypes.number.isRequired,
-    protein: PropTypes.number.isRequired,
-  }).isRequired,
-};
-
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0, 3.99),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3, 4.99),
-  createData('Eclair', 262, 16.0, 24, 6.0, 3.79),
-  createData('Cupcake', 305, 3.7, 67, 4.3, 2.5),
-  createData('Gingerbread', 356, 16.0, 49, 3.9, 1.5),
-];
-
-export default function CollapsibleTable() {
-  return (
-    <TableContainer component={Paper}>
-      <Table aria-label="collapsible table">
-        <TableHead>
-          <TableRow>
-            <TableCell />
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <Row key={row.name} row={row} />
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
-  );
-}
+/*
+ Row.propTypes = {
+   row: PropTypes.shape({
+     calories: PropTypes.number.isRequired,
+     carbs: PropTypes.number.isRequired,
+     fat: PropTypes.number.isRequired,
+     history: PropTypes.arrayOf(
+       PropTypes.shape({
+         amount: PropTypes.number.isRequired,
+         customerId: PropTypes.string.isRequired,
+         date: PropTypes.string.isRequired,
+       }),
+     ).isRequired,
+     name: PropTypes.string.isRequired,
+     price: PropTypes.number.isRequired,
+     protein: PropTypes.number.isRequired,
+   }).isRequired,
+ };
+*/
+/*Data in the rows needs to be inserted dynamically*/
