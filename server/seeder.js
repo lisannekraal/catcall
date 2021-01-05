1d45058910313a1e51dd03f4de4b68048b38d7bf
serendatapy <arvaldivia@gmail.com>
2021-01-05 23:30:50 +0100

    feat: added file to automate seeding

---
+++ server/seeder.js
@@ -0,0 +1,24 @@
+const dotenv = require('dotenv').config();
+const path = require('path');
+const { Seeder } = require('mongo-seeding');
+
+const config = {
+  database: process.env.DB_URL,
+  dropDatabase: true,
+};
+const seeder = new Seeder(config);
+const collections = seeder.readCollectionsFromPath(
+  path.resolve('./data'),
+  {
+    transformers: [Seeder.Transformers.replaceDocumentIdWithUnderscoreId],
+  },
+);
+
+seeder
+  .import(collections)
+  .then(() => {
+    console.log('Success');
+  })
+  .catch(err => {
+    console.log('Error', err);
+  });
