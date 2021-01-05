fd6e0edbb0777264f19fa4769e81aafebe6641a3
serendatapy <arvaldivia@gmail.com>
2021-01-05 22:51:31 +0100

    feat: added possibility to change the number of catcalls that can be added at the time

--- server/data/mockcatcalls/seeder.js
+++ server/data/mockcatcalls/seeder.js
@@ -1,28 +1,29 @@
 const { getObjectId } = require("mongo-seeding");
-// const catcalls = ["John", "Joanne", "Bob", "Will", "Chris", "Mike", "Anna", "Jack", "Peter", "Paul"];
-// const min = 18;
-// const max = 100;
 const faker = require('faker/locale/en_GB');
-let catcall = {
-  "geometry": {
-    "coordinates": [Number(faker.address.latitude()),Number(faker.address.longitude())],
-    "type": "Point",
-  },
-  "type": "Feature",
-  "properties": {
-    "quote": `${faker.random.number()}`,
-    "context": faker.lorem.sentence(),
-    "dateCatcall": `${Date.parse(faker.date.past())}`,
-    "dateAdded":  `${Date.parse(faker.date.recent())}`,
-    "url": "",
-    "verified": false,
-    "chalked": false,
-    "listedForChalk": false,
-    "starred": false,
-    "trash": false
-  }
-}
+const numCatcalls = 10;
 
- module.exports = catcall;
+let catcalls = [...Array(numCatcalls)].map(() => (
+  {
+    "geometry": {
+      "coordinates": [ Number(faker.address.longitude()), Number(faker.address.latitude())],
+      "type": "Point",
+    },
+    "type": "Feature",
+    "properties": {
+      "quote": `${faker.random.number()}`,
+      "context": faker.lorem.sentence(),
+      "dateCatcall": `${Date.parse(faker.date.past())}`,
+      "dateAdded": `${Date.parse(faker.date.recent())}`,
+      "url": "",
+      "verified": false,
+      "chalked": false,
+      "listedForChalk": false,
+      "starred": false,
+      "trash": false
+    }
+  }
+));
+
+module.exports = catcalls;
 
 //
