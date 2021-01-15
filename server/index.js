6bdf659417f34c080dddf746a58ee811b441c318
Lisan <lisannekraal@gmail.com>
2021-01-15 17:26:03 +0100

    adjust port server

--- server/index.js
+++ server/index.js
@@ -46,6 +46,6 @@ const server = new ApolloServer({
 
 
 
-server.listen().then(({ url }) => {
+server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
   console.log(`🚀 Server ready at ${url}`);
 });
