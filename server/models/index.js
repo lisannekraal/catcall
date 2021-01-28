d74e895089216caf0c2255eca377eee89c735658
serendatapy <arvaldivia@gmail.com>
2021-01-28 11:44:37 +0000

    refactor: removed server folder

--- server/models/index.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-const url = process.env.DB_ATLAS_URL;
-
-mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true});
-
-module.exports = mongoose;
